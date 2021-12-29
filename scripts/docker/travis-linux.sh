afca2cc0d3178e17874ae853870bdb3a99a63d34
zuosi.huang <zuosi.huang@tuya.com>
2021-12-29 20:25:05 +0800

    注释test；

--- scripts/docker/travis-linux.sh
+++ scripts/docker/travis-linux.sh
@@ -5,7 +5,9 @@ export CXXFLAGS='-ffunction-sections -fdata-sections -fvisibility-inlines-hidden
 export CFLAGS='-ffunction-sections -fdata-sections -static-libstdc++ -static-libgcc'
 export LDFLAGS='-Wl,--gc-sections'
 
-$PYTHON_BINARY setup.py --ninja --lief-test build \
+# --lief-test
+
+$PYTHON_BINARY setup.py --ninja  build \
   bdist_wheel --skip-build --dist-dir wheel_stage
 
 find wheel_stage -iname "*-cp${PYTHON_VERSION}-*" -exec auditwheel repair -w dist --plat manylinux2014_x86_64 {} \;
